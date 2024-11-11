ed9b11b651083a083a04216fb1101e335bd4d9f9
Kalle Minkner <info@kalleminkner.de>
2024-11-11 12:11:44 +0100

    fix: only build when Version.cs changed

---
+++ src/Version.cs
@@ -0,0 +1,11 @@
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Modules.Commands;
+
+namespace UpdateManager
+{
+    public partial class UpdateManager : BasePlugin
+    {
+        public override string ModuleVersion => "0.1.2";
+    }
+}
