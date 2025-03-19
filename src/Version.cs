699a75414b7b3d5bb6c6440f0c5404bde36be99f
Kalle <info@kalleminkner.de>
2025-03-19 15:26:11 +0100

    bump version to 0.1.6

--- src/Version.cs
+++ src/Version.cs
@@ -4,6 +4,6 @@ namespace UpdateManager
 {
     public partial class UpdateManager : BasePlugin
     {
-        public override string ModuleVersion => "0.1.5";
+        public override string ModuleVersion => "0.1.6";
     }
 }
